c09ad9661b6bf216ed95dd83d204c2591303f961
Jesus Castillo Hernandez <jesus.castilloh@stkprojects.com>
2019-04-16 08:56:31 -0500

    Updates

---
+++ precache-manifest.a32896b9548f8e49b36442783f0d68e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blls/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bcd5f16bbc48206973f5",
+    "url": "/blls/static/js/runtime~main.bcd5f16b.js"
+  },
+  {
+    "revision": "a8dfba59311f996058b7",
+    "url": "/blls/static/js/main.a8dfba59.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/blls/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "a8dfba59311f996058b7",
+    "url": "/blls/static/css/main.02c8c340.chunk.css"
+  },
+  {
+    "revision": "87aef8ddbbebcf57ce1aeb88946bc0e7",
+    "url": "/blls/index.html"
+  }
+];
